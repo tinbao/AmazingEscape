fae4c1b7241953bee643656b891adea3a21aa353
tinbao <tinb@student.unimelb.edu.au>
2017-05-26 22:36:36 +1000

    comments, n stuff

--- core/src/mycontroller/MyAIController.java
+++ core/src/mycontroller/MyAIController.java
@@ -1,8 +1,13 @@
 package mycontroller;
 
+import java.util.HashMap;
+
 import controller.CarController;
 import world.Car;
+import world.WorldSpatial;
 import factories.*;
+import utilities.Coordinate;
+import tiles.MapTile;
 
 public class MyAIController extends CarController{
 	
@@ -13,9 +18,19 @@ public class MyAIController extends CarController{
 	/** Moving state of the car */
 	protected CarState state;
 	
-	/** Cars move at */
+	private boolean isTurningLeft = false;
+	private boolean isTurningRight = false; 
+	
+	/** Car's top speed */
 	private final float CAR_SPEED = 3;
 	
+	/** Offset used to differentiate between 0 and 360 degrees */
+	private int EAST_THRESHOLD = 3;
+		
+	/**
+	 * Constructor for the controller (only going to be instiantised once)
+	 * @param car object representing the car
+	 */
 	public MyAIController(Car car) {
 		super(car);
 		
@@ -28,10 +43,103 @@ public class MyAIController extends CarController{
 		state = CarState.STOPPED;
 	}
 
+	/**
+	 * Update the car
+	 * @param delta time passing
+	 */
 	@Override
 	public void update(float delta) {
 		
 		
 	}
+	
+	/**
+	 * Chooses a suitable algorithm for the specific environment the car is in
+	 * @param currentView car's current viewable range
+	 */
+	public void chooseAlgorithm(HashMap<Coordinate, MapTile> currentView) {
+		
+	}
+	
+	
+	/**
+	 * Readjust the car to the orientation we are in.
+	 * @param lastTurnDirection 
+	 * @param delta time passed
+	 */
+	@SuppressWarnings("unused")
+	private void readjust(WorldSpatial.RelativeDirection lastTurnDirection, float delta) {
+		if(lastTurnDirection != null){
+			if(!isTurningRight && lastTurnDirection.equals(WorldSpatial.RelativeDirection.RIGHT)){
+				adjustRight(getOrientation(),delta);
+			}
+			else if(!isTurningLeft && lastTurnDirection.equals(WorldSpatial.RelativeDirection.LEFT)){
+				adjustLeft(getOrientation(),delta);
+			}
+		}
+		
+	}
+	
+	/**
+	 * Try to orient myself to a degree that I was supposed to be at if I am
+	 * misaligned.
+	 */
+	private void adjustLeft(WorldSpatial.Direction orientation, float delta) {
+		
+		switch(orientation){
+		case EAST:
+			if(getAngle() > WorldSpatial.EAST_DEGREE_MIN+EAST_THRESHOLD){
+				turnRight(delta);
+			}
+			break;
+		case NORTH:
+			if(getAngle() > WorldSpatial.NORTH_DEGREE){
+				turnRight(delta);
+			}
+			break;
+		case SOUTH:
+			if(getAngle() > WorldSpatial.SOUTH_DEGREE){
+				turnRight(delta);
+			}
+			break;
+		case WEST:
+			if(getAngle() > WorldSpatial.WEST_DEGREE){
+				turnRight(delta);
+			}
+			break;
+			
+		default:
+			break;
+		}
+		
+	}
 
+	private void adjustRight(WorldSpatial.Direction orientation, float delta) {
+		switch(orientation){
+		case EAST:
+			if(getAngle() > WorldSpatial.SOUTH_DEGREE && getAngle() < WorldSpatial.EAST_DEGREE_MAX){
+				turnLeft(delta);
+			}
+			break;
+		case NORTH:
+			if(getAngle() < WorldSpatial.NORTH_DEGREE){
+				turnLeft(delta);
+			}
+			break;
+		case SOUTH:
+			if(getAngle() < WorldSpatial.SOUTH_DEGREE){
+				turnLeft(delta);
+			}
+			break;
+		case WEST:
+			if(getAngle() < WorldSpatial.WEST_DEGREE){
+				turnLeft(delta);
+			}
+			break;
+			
+		default:
+			break;
+		}
+		
+	}
 }
